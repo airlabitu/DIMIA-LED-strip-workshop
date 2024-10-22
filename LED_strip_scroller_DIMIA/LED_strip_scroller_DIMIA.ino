2c1eb7813abb9c036a92df8f0709f9b9eda45481
air-lab-itu <49686201+air-lab-itu@users.noreply.github.com>
2024-10-22 13:41:36 +0200

    Add files via upload

---
+++ LED_strip_scroller_DIMIA/LED_strip_scroller_DIMIA.ino
@@ -0,0 +1,43 @@
+/* LEDstrip Scroller | AIR Lab DIMIA led strip workshop 2024
+
+   The code provides a scrolling motion on the LED strip and the ESP32
+
+   Syntax for changing color:
+   leds[number of LED] = CRGB(Red value, Green value, Blue value); (Values are 0-255)
+   FastLED.show()
+
+*/
+
+#include <FastLED.h>
+#define NUM_LEDS 10
+#define DATA_PIN 23
+CRGB leds[NUM_LEDS];
+
+int i = 0;
+long timer = 0;
+int interval = 100;
+
+void setup()
+{
+  FastLED.addLeds<NEOPIXEL, DATA_PIN>(leds, NUM_LEDS);
+  for (int i = 0; i < NUM_LEDS; i++){
+    leds[i] = CRGB(0,0,0); // turn off all LEDs in array
+  }
+  FastLED.show(); // send data to LED strip
+}
+
+
+void loop()
+{
+
+  if (millis() - timer > interval) { // the code in this block runs every interval (100 milliseconds)
+    timer = millis(); // Zero the timer to "now"
+    
+    i++;  //increment index variable
+    if (i == 10) i = 0; // reset to zero at i == 10, to match amount of LEDs on strip
+
+    FastLED.clear();
+    leds[i] = CRGB(255, 0, 100);
+    FastLED.show();
+  }
+}
